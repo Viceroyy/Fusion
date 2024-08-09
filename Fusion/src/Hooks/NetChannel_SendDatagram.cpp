9e003ccb943f3d32cab5890414ce067bf81a58c0
whohyno <90461313+whohyno@users.noreply.github.com>
2024-08-09 14:53:00 +0300

    8

--- Fusion/src/Hooks/NetChannel_SendDatagram.cpp
+++ Fusion/src/Hooks/NetChannel_SendDatagram.cpp
@@ -5,19 +5,19 @@
 MAKE_SIGNATURE(NetChannel_SendDatagram, "engine.dll", "40 55 57 41 56 48 8D AC 24", 0x0);
 
 MAKE_HOOK(NetChannel_SendDatagram, S::NetChannel_SendDatagram(), int, __fastcall,
-	CNetChannel* netChannel, bf_write* datagram)
+    CNetChannel* netChannel, bf_write* datagram)
 {
-	if (!netChannel || datagram || netChannel->GetTimeSinceLastReceived() > TICK_INTERVAL * 2 || netChannel->IsTimingOut() || netChannel->IsLoopback())
-		return CALL_ORIGINAL(netChannel, datagram);
+    if (!I::EngineClient->IsInGame() || !netChannel || datagram)
+        return CALL_ORIGINAL(netChannel, datagram);
 
-	F::Backtrack.bFakeLatency = H::Entities.GetLocal() && Vars::Backtrack::Enabled.Value && Vars::Backtrack::LatencyMode.Value == 1 || Vars::Backtrack::LatencyMode.Value == 2 && Vars::Backtrack::Latency.Value;
-	if (!F::Backtrack.bFakeLatency)
-		return CALL_ORIGINAL(netChannel, datagram);
+    F::Backtrack.bFakeLatency = Vars::Backtrack::LatencyMode.Value == 1 || Vars::Backtrack::LatencyMode.Value == 2 && Vars::Backtrack::Latency.Value;
+    if (!Vars::Backtrack::Enabled.Value || !F::Backtrack.bFakeLatency)
+        return CALL_ORIGINAL(netChannel, datagram);
 
-	const int nInSequenceNr = netChannel->m_nInSequenceNr, nInReliableState = netChannel->m_nInReliableState;
-	F::Backtrack.AdjustPing(netChannel);
-	const int original = CALL_ORIGINAL(netChannel, datagram);
-	netChannel->m_nInSequenceNr = nInSequenceNr, netChannel->m_nInReliableState = nInReliableState;
+    const int nInSequenceNr = netChannel->m_nInSequenceNr, nInReliableState = netChannel->m_nInReliableState;
+    F::Backtrack.AdjustPing(netChannel);
+    const int original = CALL_ORIGINAL(netChannel, datagram);
+    netChannel->m_nInSequenceNr = nInSequenceNr, netChannel->m_nInReliableState = nInReliableState;
 
-	return original;
+    return original;
 }
