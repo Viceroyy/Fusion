8892d64bcc7df17b765c2cb227d5534e784fb124
Gergish <g.berdnikov674@gmail.com>
2024-08-26 16:45:56 +0700

    Update TickHandler.cpp

--- Fusion/src/Features/TickHandler/TickHandler.cpp
+++ Fusion/src/Features/TickHandler/TickHandler.cpp
@@ -40,8 +40,15 @@ void CTickshiftHandler::Recharge(CUserCmd* pCmd, CTFPlayer* pLocal)
 }
 
 void CTickshiftHandler::Teleport(CUserCmd* pCmd)
+{
+	G::Warp = false;
+	if (!G::ShiftedTicks || G::DoubleTap || G::Recharge || bSpeedhack)
+		return;
 
-@@ -52,16 +52,29 @@ void CTickshiftHandler::Teleport(CUserCmd* pCmd)
+	G::Warp = Vars::CL_Move::Doubletap::Warp.Value;
+	if (G::Warp && bGoalReached)
+		G::ShiftedGoal = std::max(G::ShiftedTicks - Vars::CL_Move::Doubletap::WarpRate.Value + 1, 0);
+}
 
 void CTickshiftHandler::Doubletap(const CUserCmd* pCmd, CTFPlayer* pLocal)
 {
@@ -49,15 +56,13 @@ void CTickshiftHandler::Doubletap(const CUserCmd* pCmd, CTFPlayer* pLocal)
 		|| G::WaitForShift || G::Warp || G::Recharge || bSpeedhack
 		|| !G::CanPrimaryAttack || (G::WeaponType == EWeaponType::MELEE ? !(pCmd->buttons & IN_ATTACK) : !G::IsAttacking) || F::AutoRocketJump.iFrame != -1)
 		return;
-	G::DoubleTap = Vars::CL_Move::Doubletap::Doubletap.Value;
 
+	G::DoubleTap = Vars::CL_Move::Doubletap::Doubletap.Value;
 	if (G::DoubleTap && Vars::CL_Move::Doubletap::AntiWarp.Value)
 		G::AntiWarp = true;
 	if (G::DoubleTap && bGoalReached)
-		G::ShiftedGoal = G::ShiftedTicks - std::min(Vars::CL_Move::Doubletap::TickLimit.Value - 1, G::ShiftedTicks + 1);
+		G::ShiftedGoal = G::ShiftedTicks - std::min(Vars::CL_Move::Doubletap::TickLimit.Value - 1, G::MaxShift);
 }
-
-int CTickshiftHandler::GetTicks(CTFPlayer* pLocal)
 
 int CTickshiftHandler::GetTicks(CTFPlayer* pLocal)
 {
@@ -65,7 +70,7 @@ int CTickshiftHandler::GetTicks(CTFPlayer* pLocal)
 		return G::ShiftedTicks - G::ShiftedGoal;
 
 	if (!Vars::CL_Move::Doubletap::Doubletap.Value
-		|| G::ShiftedTicks < std::min(Vars::CL_Move::Doubletap::TickLimit.Value - 1, G::MaxShift)
+		|| G::ShiftedTicks < std::min(Vars::CL_Move::Doubletap::TickLimit.Value - 1, G::ShiftedTicks + 1)
 		|| G::WaitForShift || G::Warp || G::Recharge || bSpeedhack || F::AutoRocketJump.iFrame != -1)
 		return 0;
 
@@ -87,13 +92,9 @@ bool CTickshiftHandler::ValidWeapon(CTFWeaponBase* pWeapon)
 	case TF_WEAPON_JAR_MILK:
 	case TF_WEAPON_LUNCHBOX:
 	case TF_WEAPON_BUFF_ITEM:
-	case TF_WEAPON_ROCKETPACK:
 	case TF_WEAPON_JAR_GAS:
 	case TF_WEAPON_LASER_POINTER:
 	case TF_WEAPON_MEDIGUN:
-	case TF_WEAPON_SNIPERRIFLE:
-	case TF_WEAPON_SNIPERRIFLE_DECAP:
-	case TF_WEAPON_SNIPERRIFLE_CLASSIC:
 	case TF_WEAPON_COMPOUND_BOW:
 	case TF_WEAPON_JAR:
 		return false;
